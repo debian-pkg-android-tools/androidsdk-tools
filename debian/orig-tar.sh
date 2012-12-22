5186786c3b4edd300b096758128d8a64c0e50d52
Jakub Adam <jakub.adam@ktknet.cz>
2012-12-22 21:22:25 +0100

    libandroidsdk-androidprefs is now part of libandroidsdk-common

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -17,7 +17,7 @@ git clone -b tools_r$REVISION https://android.googlesource.com/platform/sdk andr
 VERSION=`git --git-dir=androidsdk-tools/.git log -1 --format=%cd~%h --date=short | sed s/-//g`
 echo "Version "$VERSION
 echo "Deleting not needed files ..."
-rm -fr `find androidsdk-tools -maxdepth 1 -type d ! -name androidsdk-tools ! -name androidprefs ! -name common ! -name sdkmanager ! -name sdkstats ! -name swtmenubar ! -name ddms`
+rm -fr `find androidsdk-tools -maxdepth 1 -type d ! -name androidsdk-tools ! -name common ! -name sdkmanager ! -name sdkstats ! -name swtmenubar ! -name ddms`
 rm -fr androidsdk-tools/common/tests
 rm -fr androidsdk-tools/sdkmanager/libs/sdklib/tests
 rm -fr androidsdk-tools/sdkmanager/app
@@ -27,9 +27,6 @@ rm -fr `find androidsdk-tools/sdkmanager/libs/sdklib/src/com/android/sdklib -max
 rm -fr `find androidsdk-tools/sdkmanager/libs/sdklib/src/com/android/sdklib/internal -type d ! -name internal ! -name build`
 rm -fr androidsdk-tools/sdkmanager/libs/sdklib/src/com/android/sdklib/internal/build/MakeIdentity.java
 # rm -fr `find androidsdk-tools -name ".*"` # this might be too strict in some later versions
-rm -fr androidsdk-tools/androidprefs/.classpath
-rm -fr androidsdk-tools/androidprefs/.project
-rm -fr androidsdk-tools/androidprefs/.gitignore
 rm -fr androidsdk-tools/sdkmanager/.gitignore
 rm -fr androidsdk-tools/sdkmanager/libs/sdklib/.settings
 rm -fr androidsdk-tools/sdkmanager/libs/sdklib/.classpath
