3c7a1802edb15a3f2342a39a55bae1c9afdaee60
Jakub Adam <jakub.adam@tieto.com>
2013-03-15 11:46:32 +0100

    d/orig-tar.sh update files to delete according to new source structure

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -18,14 +18,14 @@ VERSION=`git --git-dir=androidsdk-tools/.git log -1 --format=%cd~%h --date=short
 echo "Version "$VERSION
 echo "Deleting not needed files ..."
 rm -fr `find androidsdk-tools -maxdepth 1 -type d ! -name androidsdk-tools ! -name common ! -name sdkmanager ! -name sdkstats ! -name swtmenubar ! -name ddms ! -name hierarchyviewer2 ! -name traceview ! -name uiautomatorviewer`
-rm -fr androidsdk-tools/common/tests
-rm -fr androidsdk-tools/sdkmanager/libs/sdklib/tests
+rm -fr androidsdk-tools/common/src/test
+rm -fr androidsdk-tools/sdkmanager/libs/sdklib/src/test
 rm -fr androidsdk-tools/sdkmanager/app
 rm -fr androidsdk-tools/sdkmanager/libs/sdkuilib
-rm -fr androidsdk-tools/ddms/libs/ddmlib/tests androidsdk-tools/ddms/libs/ddmuilib/tests
-rm -fr `find androidsdk-tools/sdkmanager/libs/sdklib/src/com/android/sdklib -maxdepth 1 ! -name sdklib ! -name build ! -name util ! -name internal ! -name SdkConstants.java ! -name ISdkLog.java`
-rm -fr `find androidsdk-tools/sdkmanager/libs/sdklib/src/com/android/sdklib/internal -type d ! -name internal ! -name build`
-rm -fr androidsdk-tools/sdkmanager/libs/sdklib/src/com/android/sdklib/internal/build/MakeIdentity.java
+rm -fr androidsdk-tools/ddms/libs/ddmlib/src/test androidsdk-tools/ddms/libs/ddmuilib/tests
+rm -fr `find androidsdk-tools/sdkmanager/libs/sdklib/src/main/java/com/android/sdklib -maxdepth 1 ! -name sdklib ! -name build ! -name util ! -name internal ! -name SdkConstants.java ! -name ISdkLog.java`
+rm -fr `find androidsdk-tools/sdkmanager/libs/sdklib/src/main/java/com/android/sdklib/internal -type d ! -name internal ! -name build`
+rm -fr androidsdk-tools/sdkmanager/libs/sdklib/src/main/java/com/android/sdklib/internal/build/MakeIdentity.java
 # rm -fr `find androidsdk-tools -name ".*"` # this might be too strict in some later versions
 rm -fr androidsdk-tools/sdkmanager/.gitignore
 rm -fr androidsdk-tools/sdkmanager/libs/sdklib/.settings
@@ -35,8 +35,6 @@ rm -fr androidsdk-tools/common/.classpath
 rm -fr androidsdk-tools/common/.project
 rm -fr androidsdk-tools/common/.settings
 rm -fr androidsdk-tools/common/.gitignore
-rm -fr androidsdk-tools/common/tests/.project
-rm -fr androidsdk-tools/common/tests/.classpath
 rm -fr androidsdk-tools/.gitignore
 
 echo "Packaging archive into ../android-sdk-tools_$REVISION+git$VERSION.orig.tar.gz ..."
